50944566d04ae80c40ad179ef5a9d279e65b5ccb
chenbiao <149027310@qq.com>
2024-05-31 13:40:07 +0800

    init

--- mq.go
+++ mq.go
@@ -4,6 +4,11 @@ import dgctx "github.com/darwinOrg/go-common/context"
 
 type SubscribeHandler func(ctx *dgctx.DgContext, message any) error
 
+type MqAdapter interface {
+	Publisher
+	Subscriber
+}
+
 type Publisher interface {
 	Publish(ctx *dgctx.DgContext, topic string, message any) error
 	Destroy(ctx *dgctx.DgContext, topic string) error
