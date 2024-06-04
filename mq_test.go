4d02d194a83f4cf9ce74e76aa8f3a6d8ee566e19
chenbiao <149027310@qq.com>
2024-06-04 14:51:44 +0800

    RedisListAdapter

--- mq_test.go
+++ mq_test.go
@@ -26,6 +26,7 @@ func TestRedisAdapter(t *testing.T) {
 
 	dc := &dgctx.DgContext{TraceId: uuid.NewString()}
 	_ = mqAdapter.Publish(dc, topic, "hello")
+	_ = mqAdapter.Publish(dc, topic, []byte("world"))
 	_ = mqAdapter.Publish(dc, topic, map[string]string{"haha": "hehe"})
 
 	time.Sleep(time.Second)
